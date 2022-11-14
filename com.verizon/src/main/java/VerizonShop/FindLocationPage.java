1c3aadffeb48e50e1cc9afd47b480f5cb8b98191
madjidchebrine <mchebrine101@gmail.com>
2022-11-14 16:31:14 -0500

    my vrizon location

---
+++ com.verizon/src/main/java/VerizonShop/FindLocationPage.java
@@ -0,0 +1,15 @@
+package VerizonShop;
+
+import base.BasePage;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FindLocation extends BasePage {
+
+    public FindLocation(){
+        PageFactory.initElements(driver,this);}
+
+        @FindBy(xpath = "")
+
+
+}
---
+++ com.verizon/src/main/java/VerizonShop/HomeShopPage.java
@@ -0,0 +1,2 @@
+package VerizonShop;public class HomeShopingPage {
+}
